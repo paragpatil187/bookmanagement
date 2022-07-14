7dc0cb03ebaf5b0e005eaad151c0a311264a6082
parag <paragpatil187@gmail.com>
2022-07-14 11:42:13 +0530

    update filtersort

--- src/Components/FilterSort.jsx
+++ src/Components/FilterSort.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 
 const FilterSort = () => {
+  const [category, setCategory] = useState([])
+  const handleCheckbox=(e)=>{
+    const option = e.target.value
+    
+  }
   return (
-    <div>FilterSort</div>
+    <>
+      <div>Filter</div>
+      <div>
+        <input type="checkbox"  onChange={handleCheckbox} value="Novel" checked={category.includes("Novel")}></input>
+        <label>novel</label>
+
+      </div>
+      <div>
+        <input type="checkbox"
+        onChange={handleCheckbox} value="Science" checked={category.includes("Science")}></input>
+        <label>science</label>
+
+      </div>
+      <div>
+        <input type="checkbox"  onChange={handleCheckbox} value="Math" checked={category.includes("Math")}></input>
+        <label>Math</label>
+
+      </div>
+      <h3>Sort</h3>
+    </>
   )
 }
 
