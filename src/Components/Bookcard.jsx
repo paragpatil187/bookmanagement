cfa0f047e2d56823ae170828220e802ac46d8427
parag <paragpatil187@gmail.com>
2022-07-19 22:58:11 +0530

    show book image

--- src/Components/Bookcard.jsx
+++ src/Components/Bookcard.jsx
@@ -4,7 +4,7 @@ const Bookcard = ({bookData}) => {
   return (
     <div>
         <div>
-        <img src="" alt=""/>
+        <img src={bookData.cover_photo}  style={{width:"100%"}} alt=""/>
         <div>{bookData.book_name}</div>
         <div>{bookData.release_year}</div>
         <div>{bookData.category}</div>
